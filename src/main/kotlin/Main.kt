a2054ca00912553c5db8057d11d1bdd6ea901b46
muhith7 <abdulmuhithnawawi@gmail.com>
2023-05-14 23:16:19 +0700

    Function Up

--- src/main/kotlin/Main.kt
+++
@@ -1,4 +0,0 @@
-//function main atau fungsi utama adalah function yang di jalankan pada kotlin
-fun main(args: Array<String>) {
-    println("Hello World!")
-} 
---
+++ src/main/kotlin/breakAndContinue/Break.kt
@@ -0,0 +1,13 @@
+package breakAndContinue
+
+fun main() {
+    var i = 0
+
+    while (true) {
+        println("Perulangan ke $i")
+        i++
+        if (i > 1000){
+            break
+        }
+    }
+}
